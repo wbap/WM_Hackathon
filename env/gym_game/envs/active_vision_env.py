873a278fbb4a719eb821107e6559063cf7aefb14
Gideon Kowadlo <gideon@agi.io>
2020-09-15 21:03:55 +1000

    ActiveVisionEnv gaze control is working - i.e. movement of gaze, and creating an observation of fovea and periphery.

--- env/gym_game/envs/active_vision_env.py
+++ env/gym_game/envs/active_vision_env.py
@@ -3,6 +3,8 @@ from gym import spaces
 import numpy as np
 import logging
 from .pygame_env import PyGameEnv
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 class ActiveVisionEnv(PyGameEnv):
@@ -12,17 +14,26 @@ class ActiveVisionEnv(PyGameEnv):
   HUMAN = 'human'
   ARRAY = 'rgb_array'
 
+  GAZE_STEP_SIZE = 20   # number of pixels, step size of gaze movement (commanded as an action direction)
+  FOVEA_FRACTION = 0.1  # fraction of diameter, at centre of image
+  # i = 0
+
   def __init__(self, num_actions, screen_width, screen_height, frame_rate=30):
-    self.fov_fraction = 0.1  # fraction of diameter, at centre of image
-    self.gaze = np.array([0, 0])       # gaze position - (y, x) tuple
-    self.actions_start = num_actions
-    self.actions_end = num_actions + 4
+    self.fov_fraction = self.FOVEA_FRACTION
+    self.step_size = self.GAZE_STEP_SIZE
+    self.gaze = np.array([screen_width//2, screen_height//2])       # gaze position - (x, y) tuple
     self.action_2_xy = {     # map actions (integers) to x,y gaze delta
-      num_actions:   np.array([-1, 0]),
-      num_actions+1: np.array([1, 0]),
-      num_actions+2: np.array([0, -1]),
-      num_actions+3: np.array([0, 1])
+      num_actions:   np.array([-1, 0]),   # 3. left
+      num_actions+1: np.array([1, 0]),    # 4. right
+      num_actions+2: np.array([0, -1]),   # 5. up
+      num_actions+3: np.array([0, 1])     # 6. down
     }
+    self.actions_start = num_actions
+    self.actions_end = num_actions + len(self.action_2_xy)
+
+    self.img_fov = None
+    self.img_periph = None
+    self._last_observed_gaze = self.gaze
 
     super().__init__(num_actions, screen_width, screen_height, frame_rate)
 
@@ -35,8 +46,8 @@ class ActiveVisionEnv(PyGameEnv):
     logging.debug("Received action: ", action)
     # if within action scope, modify gaze
     if self.actions_start <= action < self.actions_end:
-      self.gaze = self.gaze + self.action_2_xy[action]
-      logging.debug("New gaze: ", self.gaze)
+      self.gaze = self.gaze + self.action_2_xy[action] * self.step_size
+      print("New gaze: ", self.gaze)
 
   def _create_action_space(self, num_actions):
     total_actions = num_actions + self.actions_end  # Gaze control
@@ -52,24 +63,37 @@ class ActiveVisionEnv(PyGameEnv):
       'peripheral': peripheral})
 
   def get_observation(self):
+
     img = self.render(mode='rgb_array')
+    # TODO: observed img is horizontally reflected, and rotated 90 deg ... ?
 
-    # crop to fovea (centre region)
-    h, w, ch = img.shape[0], img.shape[1], img.shape[2]
+    # if gaze position changed, then update foveal and peripheral images, unless they are undefined
+    imgs_undef = self.img_fov is None or self.img_periph is None
+    gaze_changed = (self._last_observed_gaze != self.gaze).any()
+    if imgs_undef or gaze_changed:
+      # crop to fovea (centre region)
+      h, w, ch = img.shape[0], img.shape[1], img.shape[2]
 
-    pixels_h = int(h * self.fov_fraction)
-    pixels_w = int(w * self.fov_fraction)
+      pixels_h = int(h * self.fov_fraction)
+      pixels_w = int(w * self.fov_fraction)
 
-    img_fov = img[self.gaze[0]:self.gaze[0]+pixels_h,
-                  self.gaze[1]:self.gaze[1]+pixels_w]
+      # TODO: check index is within bounds
+      self.img_fov = img[self.gaze[1]:self.gaze[1]+pixels_h, self.gaze[0]:self.gaze[0]+pixels_w, :]
 
-    # zero foveal region, and return as peripheral (other options are to blur or cut it up differently)
-    img_periph = img.copy()
-    img_periph[self.gaze[0]:self.gaze[0] + pixels_h, self.gaze[1]:self.gaze[1] + pixels_w] = 0.
+      # zero foveal region, and return as peripheral (other options are to blur or cut it up differently)
+      self.img_periph = img.copy()
+      self.img_periph[self.gaze[1]:self.gaze[1]+pixels_h, self.gaze[0]:self.gaze[0]+pixels_w, :] = 0.
+
+      self._last_observed_gaze = self.gaze.copy()
+
+      # debugging
+      # plt.imsave(str(self.i)+'_fov.png', self.img_fov)
+      # plt.imsave(str(self.i)+'_periph.png', self.img_periph)
+      # self.i += 1
 
     observation = {
       'full': img,
-      'fovea': img_fov,
-      'peripheral': img_periph}
+      'fovea': self.img_fov,
+      'peripheral': self.img_periph}
 
     return observation
