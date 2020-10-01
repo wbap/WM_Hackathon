844412060b4a4c5ed3fc325e51a0bcb91267590b
Gideon Kowadlo <gideon@agi.io>
2020-10-01 14:44:14 +1000

    Update dockerfile with necessary pip dependencies, and update script so that it can run the scripts within the container from the command line.

--- deployment/run-docker.sh
+++ deployment/run-docker.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-# usage: run-docker.sh [path to wm code] [boolean for GPU or not]
+# usage: run-docker.sh [path to wm code] [boolean for GPU or not] [command and params to run in container]
+# e.g. ../deployment/run-docker.sh ~/Dev/wm_hack False python train_stub_agent.py dm2s-v0 configs/dm2s_env.par configs/simple_agent_model.json
+# ../deployment/run-docker.sh ~/Dev/wm_hack False python keyboard_agent.py dm2s-v0 configs/dm2s_env.par
 
 export SRC_DIR=${1:-~/Dev/wm_hack}
 export GPU=${2:-False}
+
+shift
+shift
 
 TRG_DIR=/root/wm
 IMG_NAME=wbai/wm:latest
@@ -13,4 +18,11 @@ then
 	GPU_STR="--gpus all"
 fi
 
-docker run -dit --rm --name=wm $GPU_STR --mount type=bind,source=$SRC_DIR,target=$TRG_DIR $IMG_NAME
+# detached (-d) and remove it when complete (-rm)
+# With -rm, we cannot see logs after the container closed
+# cmd="docker run -dit --rm --name=wm $GPU_STR --mount type=bind,source=$SRC_DIR,target=$TRG_DIR $IMG_NAME $@"
+
+# not detached (i.e. see the output)
+cmd="docker run -it --rm --name=wm $GPU_STR --mount type=bind,source=$SRC_DIR,target=$TRG_DIR $IMG_NAME $@"
+echo $cmd
+eval $cmd
