2019f3bfbc8c01c0bdc8ba39aaeb190e684558d8
Gideon Kowadlo <gideon@agi.io>
2020-10-14 15:08:13 +1100

    Update name of docker image to download.

--- deployment/build-docker.sh
+++ deployment/build-docker.sh
@@ -3,6 +3,6 @@
 # This requires you to be in the /deployment folder when running
 # That is because it sets the context for the Dockerfile
 
-export NAME=${1:-wbai_wm_hackathon}
+export NAME=${1:-cerenaut/wbai_wm_hackathon}
 
 docker build -t $NAME:latest .
