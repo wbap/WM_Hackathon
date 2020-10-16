0540e4f244019438fa0c2379480bcc480161b2e9
Gideon Kowadlo <gideon@agi.io>
2020-10-16 12:40:13 +1100

    Add script that had not been committed and pushed.

---
+++ deployment/setupcore_and_run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# this script is used by run_in_docker.sh to first setup cerenaut-pt-core, and then execute the chosen command
+
+# install dependent project of core modules, into the environment
+cd ../../cerenaut-pt-core
+python setup.py develop
+cd ../wm_hackathon/env/
+
+source activate wm_env
+
+echo $@
+eval $@
