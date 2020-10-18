d91e9dae12b654248a322717ac0ea70c0fc95971
Gideon Kowadlo <gideon@agi.io>
2020-10-18 11:18:35 +1100

    Bugfix to Positional Encoding

--- env/gym_game/stubs/positional_encoder.py
+++ env/gym_game/stubs/positional_encoder.py
@@ -27,7 +27,7 @@ class PositionalEncoder(nn.Module):
     # input = gaze x, y
     # output = position
 
-    Based on code at: https://towardsdatascience.com/how-to-code-the-transformer-in-pytorch-24db27c8f9ec
+    Ideas from code at: https://towardsdatascience.com/how-to-code-the-transformer-in-pytorch-24db27c8f9ec
     """
 
     dims = self._config['dims']
@@ -43,17 +43,17 @@ class PositionalEncoder(nn.Module):
           pe[key][pos, i] = math.sin(pos / (10000 ** ((2 * i) / dims)))
           pe[key][pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1)) / dims)))
 
-      pe[key] = pe[key].unsqueeze(0)
+      pe[key].unsqueeze(0)
       self.register_buffer('pe_' + key, pe[key])
 
     output_shape = [-1, self._config['dims']]
     return output_shape
 
   def forward(self, xy_tensor):
-    x = xy_tensor[0][0]
-    y = xy_tensor[0][1]
-    pe_x = Variable(self.pe_x[x, :], requires_grad=False).cuda()
-    pe_y = Variable(self.pe_y[y, :], requires_grad=False).cuda()
+    x = int(xy_tensor[0][0])
+    y = int(xy_tensor[0][1])
+    pe_x = self.pe_x[x, :]
+    pe_y = self.pe_y[y, :]
     return pe_x, pe_y
 
   def get_output_shape(self):
